dd0c61f2e0439f3e7b030d7f5f30bf60689e9af2
Uryel <uryelhenrique.c@gmail.com>
2019-02-13 14:53:37 +0000

    Remove unesed code

--- app/src/main/java/com/uhc/my_albums/di/modules/AppModule.kt
+++ app/src/main/java/com/uhc/my_albums/di/modules/AppModule.kt
@@ -2,9 +2,7 @@ package com.uhc.my_albums.di.modules
 
 import android.app.Application
 import android.arch.lifecycle.ViewModelProvider
-import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Room
-import android.arch.persistence.room.migration.Migration
 import com.uhc.my_albums.data.source.local.AlbumsDao
 import com.uhc.my_albums.data.source.local.Database
 import com.uhc.my_albums.ui.list.AlbumsViewModelFactory
@@ -18,15 +16,6 @@ import javax.inject.Singleton
 @Module
 class AppModule(val app: Application) {
 
-    companion object {
-        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                // Change the table name to the correct one
-                database.execSQL("ALTER TABLE album RENAME TO albums")
-            }
-        }
-    }
-
     @Provides
     @Singleton
     fun provideApplication(): Application = app
@@ -37,7 +26,6 @@ class AppModule(val app: Application) {
         app,
         Database::class.java, Constants.DATABASE_NAME
     )
-        /*.addMigrations(MIGRATION_1_2)*/
         .fallbackToDestructiveMigration()
         .build()
 
